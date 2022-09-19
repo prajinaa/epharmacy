f478e7a113e02063e5afea6992839623dc9a9908
prajinaa <102461459+prajinaa@users.noreply.github.com>
2022-09-19 22:39:14 +0545

    Update auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -2,12 +2,18 @@
 // import 'dart:js';
 // import 'lib.dart';
 
+import 'dart:convert';
+
+// import 'package:epharmacy/features/home/screen/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:epharmacy/constant/error_handling.dart';
 import 'package:epharmacy/constant/global_variables.dart';
 import 'package:epharmacy/constant/utils.dart';
 import 'package:epharmacy/models/user.dart';
 import 'package:http/http.dart' as http;
+// import 'package:epharmacy/providers/user_provider.dart';
+// import 'package:provider/provider.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 // allow make api reques
 
 class AuthService {
@@ -51,4 +57,80 @@ class AuthService {
         showSnackBar(context, e.toString());
        }
   }
+
+
+void loginUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/login'),
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () async {
+          // SharedPreferences prefs = await SharedPreferences.getInstance();
+          // Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          // await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+          // Navigator.pushNamedAndRemoveUntil(
+          //   context,
+          //   HomeScreen.routeName,
+          //   (route) => false,
+          // );
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+ }
 }
+
+  // // get user data
+  // void getUserData(
+  //   BuildContext context,
+  // ) async {
+  //   try {
+  //     SharedPreferences prefs = await SharedPreferences.getInstance();
+  //     String? token = prefs.getString('x-auth-token');
+
+  //     if (token == null) {
+  //       prefs.setString('x-auth-token', '');
+  //     }
+
+  //     var tokenRes = await http.post(
+  //       Uri.parse('$uri/tokenIsValid'),
+  //       headers: <String, String>{
+  //         'Content-Type': 'application/json; charset=UTF-8',
+  //         'x-auth-token': token!
+  //       },
+  //     );
+
+  //     var response = jsonDecode(tokenRes.body);
+
+  //     if (response == true) {
+  //       http.Response userRes = await http.get(
+  //         Uri.parse('$uri/'),
+  //         headers: <String, String>{
+  //           'Content-Type': 'application/json; charset=UTF-8',
+  //           'x-auth-token': token
+  //         },
+  //       );
+
+  //       var userProvider = Provider.of<UserProvider>(context, listen: false);
+  //       userProvider.setUser(userRes.body);
+//       }
+//     } catch (e) {
+//       showSnackBar(context, e.toString());
+//     }
+//   }
+// }
